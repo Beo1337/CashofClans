4760e2b1154e101edc683c2f81ca128bc0d071af
SON GOHAN <J.Finkenstaedt@gmail.com>
2017-04-13 14:24:03 +0200

    DB

--- app/src/main/java/com/example/seps/cashofclans/DatabaseHelper.java
+++ app/src/main/java/com/example/seps/cashofclans/DatabaseHelper.java
@@ -12,7 +12,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "cash.db";
     public static final String TABLE_NAME_MAIN = "UEBERSICHT";
-    public static final String COL_ID = "ID";
+    /*public static final String COL_ID = "ID";
     public static final String COL_BETRAG = "BETRAG";
     public static final String COL_TITEL = "TITEL";
     public static final String COL_KATEGORIE = "KATEGORIE";
@@ -23,6 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_CATEGORY = "KATEGORIE";
     public static final String CAT_ID = "ID";
     public static final String CAT_NAME ="NAME";
+    */
 
 
     public DatabaseHelper(Context context) {
@@ -32,14 +33,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // NICHT SICHER BEI GPS UND FOTO TYPE, SELBIGES GILT FÜR KATEGORIE ICON
-        db.execSQL("CREATE TABLE "+TABLE_NAME_MAIN+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, BETRAG FLOAT, TITEL TEXT, KATEGORIE TEXT, DATUM DATE, GPS TEXT, FOTO TEXT)");
-        db.execSQL("CREATE TABLE "+TABLE_CATEGORY+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ICON TEXT)");
+        // Typen der Spalten stimmen noch nicht
+        db.execSQL("CREATE TABLE "+TABLE_NAME_MAIN+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, BETRAG TEXT, TITEL TEXT, KATEGORIE TEXT, DATUM TEXT, GPS TEXT, FOTO TEXT)");
+        //db.execSQL("CREATE TABLE "+TABLE_CATEGORY+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ICON TEXT)");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME_MAIN);
         onCreate(db);
     }
 }
