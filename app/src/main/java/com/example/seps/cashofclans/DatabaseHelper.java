96c60ca63078ba259294faa29fefaaf33182bf60
SON GOHAN <J.Finkenstaedt@gmail.com>
2017-04-18 17:12:48 +0200

    DB

--- app/src/main/java/com/example/seps/cashofclans/DatabaseHelper.java
+++ app/src/main/java/com/example/seps/cashofclans/DatabaseHelper.java
@@ -10,38 +10,33 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "cash.db";
-    public static final String TABLE_NAME_MAIN = "UEBERSICHT";
-    /*public static final String COL_ID = "ID";
-    public static final String COL_BETRAG = "BETRAG";
-    public static final String COL_TITEL = "TITEL";
-    public static final String COL_KATEGORIE = "KATEGORIE";
-    public static final String COL_DATE = "DATUM";
-    public static final String COL_GPS = "GPS";
-    public static final String COL_FOTO = "FOTO";
 
-    public static final String TABLE_CATEGORY = "KATEGORIE";
-    public static final String CAT_ID = "ID";
-    public static final String CAT_NAME ="NAME";
-    */
+        // Database Version
+        private static final int DATABASE_VERSION = 1;
+        private static final String DATABASE_NAME = "COC_DB";
+        // Contacts table name
+        private static final String TABLE_SHOPS = "UEBERSICHT";
+        // Shops Table Columns names
+        private static final String KEY_ID = "ID";
+        private static final String KEY_NAME = "name";
+        private static final String KEY_BETRAG = "betrag";
 
 
-    public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+        public DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_SHOPS + "("
+            + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+            + KEY_BETRAG + "NUMBER" + ")";
+            db.execSQL(CREATE_CONTACTS_TABLE);
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+// Drop older table if existed
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_SHOPS);
+// Creating tables again
+            onCreate(db);
+        }
     }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        // Typen der Spalten stimmen noch nicht
-        db.execSQL("CREATE TABLE "+TABLE_NAME_MAIN+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, BETRAG TEXT, TITEL TEXT, KATEGORIE TEXT, DATUM TEXT, GPS TEXT, FOTO TEXT)");
-        //db.execSQL("CREATE TABLE "+TABLE_CATEGORY+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ICON TEXT)");
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME_MAIN);
-        onCreate(db);
-    }
-}
