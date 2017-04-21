e7622235276642794f24a461570f74a0423b0852
Beo1337 <simonstaudinger@gmx.at>
2017-04-21 21:42:41 +0200

    Datenbank
    Kategorien werden gespeichert.
    Kategorieshortcuts funktionieren.
    Datenbankinhalt kann über shortcut angezeigt werden.

--- app/src/main/java/com/example/seps/cashofclans/DatabaseHelper.java
+++ app/src/main/java/com/example/seps/cashofclans/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.seps.cashofclans;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -18,23 +19,58 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME_REPEAT_ENTRY = "REPEAT_ENTRY";
 
 
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        db.execSQL("CREATE TABLE "+TABLE_NAME_MAIN+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, BETRAG NUMBER(10,2), TITEL TEXT, KATEGORIE INTEGER, DATUM TEXT, GPS TEXT, FOTO TEXT)");
-        db.execSQL("CREATE TABLE "+TABLE_NAME_CATEGORY+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ICON TEXT)");
-
+        Log.i("DatabaseHelper","onCreate!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        db.execSQL("CREATE TABLE "+TABLE_NAME_MAIN+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, BETRAG NUMBER(10,2), TITEL TEXT, KATEGORIE INTEGER, DATUM DATE, GPS TEXT, FOTO TEXT, FOREIGN KEY(KATEGORIE) REFERENCES category(ID))");
+        db.execSQL("CREATE TABLE "+TABLE_NAME_CATEGORY+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT , ICON TEXT, CONSTRAINT name_unique UNIQUE (NAME))");
+        addCategory("Lebensmittel", db);
+        addCategory("Bar", db);
+        addCategory("Sport", db);
+        addCategory("Kleidung", db);
+        addCategory("Bücher", db);
+        addCategory("Kino", db);
+        addCategory("Gehalt",db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i("DatabaseHelper","onUpgrade!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME_MAIN);
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME_CATEGORY);
         onCreate(db);
+
     }
+
+    public void addCategory (String name, SQLiteDatabase db){
+
+        //Werte für Datenbank vorbereiten
+        ContentValues values = new ContentValues();
+        values.put("NAME", name);
+
+        //In die Datenbank speichern
+        long newRowId = db.insert("category", null, values);
+        Log.i("DatabaseHelper", "Eingefügt " + newRowId);
+    }
+
+    public void addCategory (String name, String icon, SQLiteDatabase db) {
+
+        //Werte für Datenbank vorbereiten
+        ContentValues values = new ContentValues();
+        values.put("NAME", name);
+        values.put("ICON",icon);
+
+        //In die Datenbank speichern
+        long newRowId = db.insert("category", null, values);
+        Log.i("DatabaseHelper", "Eingefügt " + newRowId);
+
+
+    }
+
 }
--- app/src/main/java/com/example/seps/cashofclans/MainActivity.java
+++ app/src/main/java/com/example/seps/cashofclans/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
     TextView betrag;
-    double summe = 0;
+
 
 
     @Override
@@ -23,10 +23,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new DatabaseHelper(this);
+        refresh_money();
 
+
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        refresh_money();
+    }
+
+    public void refresh_money(){
         SQLiteDatabase db = myDb.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM uebersicht", null);
         Log.i("MainActivity:","Anzahl Daten: "+cursor.getCount());
+        double summe = 0;
 
         while(cursor.moveToNext())
         {
@@ -39,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
             betrag.setTextColor(Color.RED);
         else
             betrag.setTextColor(Color.GREEN);
-
     }
 
     public void einstellungen(View v) {
@@ -51,19 +62,66 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(v.getContext(), AddActivity.class);
         i.putExtra("mode", "add");
         startActivityForResult(i, 0);
-        finish();
     }
 
     public void sub(View v){
         Intent i = new Intent(v.getContext(), AddActivity.class);
         i.putExtra("mode", "sub");
         startActivityForResult(i, 0);
-        finish();
+    }
+
+    public void grocery(View v){
+        Intent i = new Intent(v.getContext(), AddActivity.class);
+        i.putExtra("mode", "sub");
+        i.putExtra("cat", "Lebensmittel");
+        startActivityForResult(i, 0);
+    }
+
+    public void club(View v){
+        Intent i = new Intent(v.getContext(), AddActivity.class);
+        i.putExtra("mode", "sub");
+        i.putExtra("cat", "Bar");
+        startActivityForResult(i, 0);
+    }
+
+    public void sport(View v){
+        Intent i = new Intent(v.getContext(), AddActivity.class);
+        i.putExtra("mode", "sub");
+        i.putExtra("cat", "Sport");
+        startActivityForResult(i, 0);
+    }
+
+    public void clothes(View v){
+        Intent i = new Intent(v.getContext(), AddActivity.class);
+        i.putExtra("mode", "sub");
+        i.putExtra("cat", "Kleidung");
+        startActivityForResult(i, 0);
+    }
+
+    public void books(View v){
+        Intent i = new Intent(v.getContext(), AddActivity.class);
+        i.putExtra("mode", "sub");
+        i.putExtra("cat", "Bücher");
+        startActivityForResult(i, 0);
+    }
+
+    public void cinema(View v){
+        Intent i = new Intent(v.getContext(), AddActivity.class);
+        i.putExtra("mode", "sub");
+        i.putExtra("cat", "Kino");
+        startActivityForResult(i, 0);
     }
 
     public void stats(View v) {
         Intent i = new Intent(v.getContext(), StatistikActivity.class);
         startActivityForResult(i, 0);
     }
+
+    public void db(View v){
+        Intent i = new Intent(v.getContext(), ShowDataBaseActivity.class);
+        startActivityForResult(i, 0);
+    }
+
+
 
 }
