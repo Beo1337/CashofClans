c0320f787c20b9948fadf90c5fd44b34d62f34a0
Michael Hackl <derkokainarienvogel@gmail.com>
2017-06-04 13:43:32 +0200

    Removed lambda =(

--- app/src/main/java/com/cashify/category/CategoryDetailActivity.java
+++ app/src/main/java/com/cashify/category/CategoryDetailActivity.java
@@ -1,8 +1,12 @@
 package com.cashify.category;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -31,12 +35,31 @@ public class CategoryDetailActivity extends AppCompatActivity {
             catNameField = (EditText) findViewById(R.id.category_detail_name);
             catNameField.setText(category.getName());
 
-            delButton = (Button) findViewById(R.id.category_detail_delete);
-            delButton.setOnClickListener(l -> {
-                catManager.removeCategory(category);
-                this.finish();
-            });
+            CategoryDetailActivity parent = this;
 
+            delButton = (Button) findViewById(R.id.category_detail_delete);
+
+            delButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+
+                    builder.setTitle("Kategorie löschen")
+                            .setIcon(R.drawable.delete_x)
+                            .setMessage(category.getName() + " wirklich löschen?")
+                            .setPositiveButton("Löschen", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    catManager.removeCategory(category);
+                                    parent.finish();
+                                }
+                            })
+                            .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            })
+                            .show();
+                }
+            });
         } catch (Exception e) {
 
         }
