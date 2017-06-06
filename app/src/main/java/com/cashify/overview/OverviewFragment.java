06b3eae96c5ce88b58b911a802862c2cd01b9de0
Beo1337 <simonstaudinger@gmx.at>
2017-06-06 16:10:09 +0200

    CSV-Export von Einträgen

--- app/src/main/java/com/cashify/overview/OverviewFragment.java
+++ app/src/main/java/com/cashify/overview/OverviewFragment.java
@@ -1,18 +1,27 @@
 package com.cashify.overview;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cashify.R;
 import com.cashify.category.Category;
@@ -22,13 +31,20 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
+import com.opencsv.CSVWriter;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class OverviewFragment extends Fragment {
 
@@ -37,6 +53,8 @@ public class OverviewFragment extends Fragment {
     private DatabaseHelper dbHelper;        // Database helper object to retrieve database entries
     private SharedPreferences sharedPref;   // Shared preferences
     private PieChart pieChart;              // Fancy pie chart!!
+    /**Über den Manager werden die Einträge aus der Datenbank ausgelesen.*/
+    private OverviewManager manager;
 
     // Context for Database helper (and others) provided through onAttach event
     @Override
@@ -58,6 +76,7 @@ public class OverviewFragment extends Fragment {
         super.onStart();
 
         Button listButton;
+        Button csvButton;
 
         listButton = (Button) getActivity().findViewById(R.id.list_button);
         listButton.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +84,14 @@ public class OverviewFragment extends Fragment {
             public void onClick(View v) {
                 Intent i = new Intent(getActivity(), OverviewListActivity.class);
                 OverviewFragment.this.startActivity(i);
+            }
+        });
+
+        csvButton = (Button) getActivity().findViewById(R.id.csv_button);
+        csvButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exportData();
             }
         });
 
@@ -133,4 +160,51 @@ public class OverviewFragment extends Fragment {
         pieChart.setHoleColor(Color.parseColor("#85a8e5"));
         pieChart.setDescription("Ausgaben in €");
     }
+
+    /**Diese Methode schreibt die Einträge der Tabell in ein CSV-File*/
+    public void exportData() {
+        DatabaseHelper myDb = new DatabaseHelper(getContext());
+        manager = new OverviewManager(myDb);
+
+        MarshMallowPermission mmp = new MarshMallowPermission(OverviewFragment.this.getActivity());
+        if (!mmp.checkPermissionForExternalStorage())//Wenn die Berechtigung noch nicht vorhanden ist
+            mmp.requestPermissionForExternalStorage();
+        else {//Wenn die Berechtigung schon vorhanden ist
+            File exportDir = new File(Environment.getExternalStorageDirectory(), "Cashify");
+            if (!exportDir.exists()) {
+                exportDir.mkdirs();//Ordner Cashify anlegen wenn er noch nicht existiert.
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH mm ss");
+            String strDate = sdf.format(new Date());
+            File file = new File(exportDir, "Chashify_Export_" + strDate + ".csv");
+
+            try {
+                file.createNewFile();//neues CSV-File erstellen
+
+                CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+                List<com.cashify.overview.Entry> ent = manager.getEntries();
+
+
+                String header[] = {"ID", "Titel", "Betrag", "Kategorie", "Datum"};//Überschriften des CSV-Files
+
+                csvWrite.writeNext(header);
+
+                //CSV-File mit Einträgen befüllen
+                Set<com.cashify.overview.Entry> set = myDb.getEntries();
+                for (com.cashify.overview.Entry e : set) {
+                    String[] text = {String.valueOf(e.getId()), e.getTitle(), String.valueOf(e.getAmount()), e.getCategory().getName(), e.getDatum()};
+                    csvWrite.writeNext(text);
+                }
+                Toast.makeText(getContext(), "File erstellt /Cashify/Chasify_Export_Datum.csv", Toast.LENGTH_SHORT).show();
+                csvWrite.close();
+                Log.d("OverviewFragment", "Fertig mit Export!!");
+            } catch (SQLException sqlEx) {
+                Log.e("OverviewFragment", sqlEx.getMessage(), sqlEx);
+            } catch (IOException e) {
+                Log.e("OverviewFragment", e.getMessage(), e);
+            }
+        }
+    }
+
+
 }
